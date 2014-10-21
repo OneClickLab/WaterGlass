e4f4fdf8b7d4c1649147859222f1adb52e85af3d
Sonny <mail@sonnyt.com>
2014-10-20 23:41:01 -0400

    Initial release

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require( 'express' ),
+    path = require( 'path' ),
+    app = express();
+
+app.use( express.static( './public' ) );
+
+app.get( '/*', function ( req, res ){
+    res.sendFile( path.join( __dirname, 'views/index.html') );
+});
+
+var server = app.listen( 5000, function() {
+    console.log( 'Listening on port %d', server.address().port );
+});
